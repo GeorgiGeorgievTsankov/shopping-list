4b00939eaed1b5cab37140fdb14ebe2218c679fc
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2023-12-30 16:07:07 +0200

    Update TodoItems.jsx

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-import './css/TodoItems.css'
 
-export const TodoItems = () => {
-  return (
-    <div>TodoItems</div>
-  )
+import check from '../assets/check.svg'
+import circle from '../assets/circle.svg'
+import delIcon from '../assets/delIcon.svg'
+
+export const TodoItems = ({no,display,text}) => {
+    return (
+
+        <div className="todoItems">
+            <div className="todoItemsContainer">
+                { display === 'none' ?<img src={check} alt="checkImage" />:<img src={circle} alt="circleImage" />}  
+                <div className="todoItemText">{text}</div>
+            </div>
+            <img src={delIcon} alt="delImage"/>
+
+        </div >
+    )
 }
+
