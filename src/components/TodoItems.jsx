9a14c21cd14d1cb3ad3ad342d8bc99f476afdade
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2023-12-31 17:49:46 +0200

    Update TodoItems.jsx

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -4,17 +4,39 @@ import check from '../assets/check.svg'
 import circle from '../assets/circle.svg'
 import delIcon from '../assets/delIcon.svg'
 
-export const TodoItems = ({no,display,text}) => {
+export const TodoItems = ({ no, display, text, setTodos }) => {
+
+    const deleteTodos = (no) =>{
+        let data = JSON.parse(localStorage.getItem('todos'));
+        data = data.filter((todo) => todo.no!==no)
+        setTodos(data);
+    }
+
+    const toggle = (no) => {
+        let data = JSON.parse(localStorage.getItem('todos'));
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].no === no) {
+                if (data[i].display === "") {
+                    data[i].display = "line-trough";
+                   
+                } else {
+                    data[i].display = "";
+                }
+                break; 
+            }
+        }
+
+        setTodos(data);
+    }
+
+
     return (
-
         <div className="todoItems">
-            <div className="todoItemsContainer">
-                { display === 'none' ?<img src={check} alt="checkImage" />:<img src={circle} alt="circleImage" />}  
-                <div className="todoItemText">{text}</div>
+            <div className="todoItemsContainer" onClick={() => { toggle(no) }}>
+                {display === "" ? <img src={circle} alt="circleImage" />:<img src={check} alt="checkImage" />}
+                {display === "" ?<div className="todoItemText">{text}</div>:<div className="activity">{text}</div>}
             </div>
-            <img src={delIcon} alt="delImage"/>
-
+            <img src={delIcon} alt="delImage" onClick={()=> deleteTodos(no)}/>
         </div >
     )
 }
-
